c9e06591e30fcd841e37212bfd65373ca4fac923
Harsha Vardhan <harshavardhan@Harshas-MacBook-Pro.local>
2020-10-22 18:32:53 -0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BackendService } from './services/backend.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private backend: BackendService) { }
+  ngOnInit() {
+   
+    this.backend.getConfig().subscribe(
+      res => {
+        console.log('RESPONSE', res)
+      }
+    );
+  }
   title = 'angular-spring-project';
 }
